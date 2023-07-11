0b31b46af45838da19557f18f07f63371873938f
Gold John King <GoldJohnKing@Live.cn>
2023-07-11 23:49:14 +0800

    Refactor server restart timer

--- Golden_Scripts/restart_timer.lua
+++ Golden_Scripts/restart_timer.lua
@@ -1,47 +1,59 @@
--- Scheduled Restart
+-- Server Restart Timer Function
 
-local restartTime = 14400 -- 4 hours
-local restartHintTime = { 60, 180, 300, 900 }
+local function restartTimer(restartTime, restartHintTimeList, restartFlag, restartHint, restartHintLastsTime)
+    for key, value in pairs(restartHintTimeList) do -- Restart hint
+        mist.scheduleFunction(function(event, sender)
+            trigger.action.outText(string.format(restartHint, value / 60), restartHintLastsTime)
+        end, {}, timer.getTime() + restartTime - value)
+    end
 
-for key, value in pairs(restartHintTime) do -- Restart hint
-	mist.scheduleFunction(function(event, sender)
-		trigger.action.outText("服务器将于" .. value / 60 .. "分钟后定时重启！", 90)
-	end, {}, timer.getTime() + restartTime - value)
+    mist.scheduleFunction(function(event, sender)
+        trigger.action.outText("服务器即将重启！", 180)
+    end, {}, timer.getTime() + restartTime - 15)
+
+    mist.scheduleFunction(function(event, sender) -- Restart
+        trigger.action.setUserFlag(restartFlag, true)
+    end, {}, timer.getTime() + restartTime)
 end
 
-mist.scheduleFunction(function(event, sender)
-	trigger.action.outText("服务器即将定时重启！", 180)
-end, {}, timer.getTime() + restartTime - 15)
+-- Server Restart Timer Function Done
 
-mist.scheduleFunction(function(event, sender) -- Restart
-	trigger.action.setUserFlag("FLAG_MISSION_RESTART", true)
-end, {}, timer.getTime() + restartTime)
+-- Scheduled Restart
+
+local scheduledRestartTime = 14400 -- 4 hours
+local scheduledRestartHintTimeList = { 60, 180, 300, 600, 900, 1200, 1500, 1800 }
+local scheduledRestartFlag = "FLAG_MISSION_RESTART"
+local scheduledRestartHint = "服务器将于 %d 分钟后定时重启！"
+
+restartTimer(scheduledRestartTime, scheduledRestartHintTimeList, scheduledRestartFlag, scheduledRestartHint, 90)
 
 -- Scheduled Restart Done
 
--- Mission Complete Check
+-- Mission Complete Restart
 
-local missionCompleteCheckSheduler = nil
+local missionCompleteCheckScheduler = nil
 
 local missionCompleteCheck = function(event, sender)
-	if BattlefieldManager.noRedZones or BattlefieldManager.noBlueZones then
-		trigger.action.outText("服务器将于90秒后清档重启！", 90)
-		mist.removeFunction(missionCompleteCheckSheduler)
+    if BattlefieldManager.noRedZones or BattlefieldManager.noBlueZones then
+        local restartTime = 1800 -- 30 minutes
+        local restartHintTimeList = { 60, 180, 300, 600, 900, 1200, 1500, 1800 }
+        local restartFlag = "FLAG_MISSION_RESTART"
+        local restartHint = "服务器将于 %d 分钟后清档重启！"
 
-		mist.scheduleFunction(function(event, sender)
-			trigger.action.outText("服务器即将清档重启！", 180)
-		end, {}, timer.getTime() + 75)
+        restartTimer(restartTime, restartHintTimeList, restartFlag, restartHint, 90)
 
-		mist.scheduleFunction(function(event, sender)
-			if lfs then
-				os.remove(lfs.writedir() .. "Missions/Saves/pretense_1.1.json")
-			end
-			
-			trigger.action.setUserFlag("FLAG_MISSION_RESTART", true)
-		end, {}, timer.getTime() + 90)
-	end
+        mist.scheduleFunction(function(event, sender)
+            if lfs then
+                os.remove(lfs.writedir() .. "Missions/Saves/pretense_1.1.json")
+            end
+
+            trigger.action.setUserFlag("FLAG_MISSION_RESTART", true)
+        end, {}, timer.getTime() + restartTime)
+
+        mist.removeFunction(missionCompleteCheckScheduler)
+    end
 end
 
-missionCompleteCheckSheduler = mist.scheduleFunction(missionCompleteCheck, {}, timer.getTime() + 90, 90)
+missionCompleteCheckScheduler = mist.scheduleFunction(missionCompleteCheck, {}, timer.getTime() + 90, 90)
 
--- Mission Complete Check Done
+-- Mission Complete Restart Done
