51d286c6c93c36e0c5b5997a0e2543dadba27736
Gold John King <GoldJohnKing@Live.cn>
2024-05-14 18:01:25 +0800

    Fix crash if zn is nil

--- Golden_Scripts/pretense_compiled.lua
+++ Golden_Scripts/pretense_compiled.lua
@@ -7178,7 +7178,7 @@ do
                 env.info('PlayerTracker - '..player..' checking if landed: '..tostring(isLanded))
 
                 if isLanded then
-                    if zn.isCarrier then
+                    if zn and zn.isCarrier then -- Edited, fix nil issue
                         zn:addResource(Config.carrierSpawnCost)
                     else
                         zn:addResource(Config.zoneSpawnCost)
